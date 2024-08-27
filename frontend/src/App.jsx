437cc43bd9564c60d266285e77e4c03fb7a923c9
nikoKhab <nmangori.1@gmail.com>
2024-08-27 13:52:50 +0400

    final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 // import { FcApproval } from "react-icons/fc";
 import { useEffect, useRef, useState } from "react";
 import './App.css';
-function App() {
 
+function App() {
   const inputRef = useRef(null);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const getTasks = async() => {
+    const getTasks = async () => {
       try {
         const res = await fetch('http://localhost:3000/tasks');
         const data = await res.json();
@@ -15,16 +15,14 @@ function App() {
       } catch (err) {
         console.log(err);
       }
-   
-    }
-    getTasks()
-  }, [])
+    };
+    getTasks();
+  }, []);
 
-  
-  const handleAddTask = async() => {
+  const handleAddTask = async () => {
     try {
       const res = await fetch(`http://localhost:3000/tasks/create/${inputRef.current.value}`, {
-        method: 'POST'
+        method: 'POST',
       });
       const data = await res.json();
       setTasks([...tasks, data]);
@@ -32,63 +30,84 @@ function App() {
       console.log(err);
     }
     inputRef.current.value = '';
+    inputRef.current.focus();
   };
-  const deleteTask = async(task) =>{
-    try{
-      const response = await fetch(`http://localhost:3000/tasks/delete/${task._id}`, {
+
+  const deleteTask = async (task) => {
+    try {
+      await fetch(`http://localhost:3000/tasks/delete/${task._id}`, {
         method: 'DELETE',
-      })
-      const data = await response.json();
-      setTasks(tasks.filter(t => t._id !== task._id));
-    }
-    catch(err){
+      });
+      setTasks(tasks.filter((t) => t._id !== task._id));
+    } catch (err) {
       console.log(err);
     }
-  }
-  const toggleDone = async(task) => {
-    try{
-      const response = await fetch(`http://localhost:3000/tasks/update/${task._id}`, {
+  };
+
+  const toggleDone = async (task) => {
+    try {
+      await fetch(`http://localhost:3000/tasks/update/${task._id}`, {
         method: 'PUT',
-      })
-      const data = await response.json();
+      });
       setTasks(
-        tasks.map((t) => t._id === task._id ? {...t, isDone: !t.isDone} : t)
+        tasks.map((t) =>
+          t._id === task._id ? { ...t, isDone: !t.isDone } : t
+        )
       );
-    }
-    catch(err){
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <>
       <div className="main">
         <div className="input-container">
-          <input
-            ref={inputRef}
-            className="input"
-            type="text"
-          />
-          <button onClick={handleAddTask}>Add Task</button>
+          <input ref={inputRef} className="input" type="text" />
+          <button onClick={handleAddTask} className="addBtn">
+            Add Task
+          </button>
         </div>
-        {tasks.length > 0 ?
+        {tasks.length > 0 ? (
           <ul>
-            {tasks.map((task, index) => <li
-            key={index}
-            className={task.isDone ? 'done' : 'task'}
-            >
-            {task.title}
-            <button onClick={() => deleteTask(task)}>Delete</button>
-            <button onClick={() => toggleDone(task)}>toggle done</button>
-            </li>)}
+            {tasks.map((task, index) => (
+              <li
+                key={index}
+                className={task.isDone ? 'done' : 'task'}
+              >
+                {task.title}
+                <div className="btnCont">
+                  <button
+                    onClick={() => deleteTask(task)}
+                    className="deleteBtn"
+                  >
+                    Delete
+                  </button>
+                  {!task.isDone ? (
+                    <button
+                      onClick={() => toggleDone(task)}
+                      className="doneBtn"
+                    >
+                      mark done
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => toggleDone(task)}
+                      className="undoneBtn"
+                    >
+                      mark undone
+                    </button>
+                  )}
+                </div>
+              </li>
+            ))}
           </ul>
-          :
+        ) : (
           <h2>No tasks found</h2>
-        }
+        )}
       </div>
     </>
   );
 }
-
 
 export default App;
